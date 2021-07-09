15e91fce8801214964db6d6d9ad26a0fa0dcb0db
aksoym <aksoym15@itu.edu.tr>
2021-07-09 20:23:06 +0300

    added cl/cm - alpha/beta figures with references

--- unit_tests.py
+++ unit_tests.py
@@ -1,8 +1,10 @@
 from update_motion import update_motion
 import time
 import numpy as np
-from aero_model_to_replace import coefficient
+from aero_model_to_replace import get_aero_forces, get_aero_force_coefficients, get_aero_moment_coefficients, _coefficient
 import pickle
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 def update_motion_unit_test(function):
@@ -41,8 +43,43 @@ def aero_coefficient_unit_test(function, label):
     print(function(label, point))
     return None
 
-with open('data/ParsedAeroDataBPs.pkl', 'rb') as f:
+with open('data/ParsedAeroDataValues.pkl', 'rb') as f:
     breakpoints = pickle.load(f)
 
 
-print(breakpoints['CY'].keys())
+density = 1.225
+velocity = 200
+alpha, beta = 20, 0
+p, q, r = 0, 0, 0
+deltaE, deltaA, deltaR, deltaLEF, deltaSB = 0, 0, 0, 0, 0
+
+
+cl_values = []
+alpha_list = np.arange(0, 40)
+deltaE_list = [0, 10, 25]
+
+for alpha in alpha_list:
+    coef = get_aero_force_coefficients(velocity, alpha, beta, p, q, r, deltaE, deltaA, deltaR, deltaLEF, deltaSB)[3]
+    cl_values.append(coef)
+
+
+def plot():
+    plt.figure(figsize=(6, 8))
+    plt.plot(alpha_list, cl_values)
+
+    plt.xlim(0, 40)
+    plt.ylim(0, 2.0)
+    plt.xlabel('alpha, deg')
+    plt.ylabel('CL')
+    plt.axhline(color='grey')
+    plt.savefig('figures/cl_alpha.png')
+    plt.show()
+
+plot()
+
+#print(breakpoints['Cm'].keys())
+
+
+
+
+
